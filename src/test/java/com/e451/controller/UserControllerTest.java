45bf2c7ddce7236a1be749e28f480e48a45b38f4
tomd8451 <tom.delaney@8451.com>
2017-03-18 19:03:19 -0400

    Closes #23

--- src/test/java/com/e451/controller/UserControllerTest.java
+++ src/test/java/com/e451/controller/UserControllerTest.java
@@ -1,14 +1,20 @@
 package com.e451.controller;
 
 
+import com.e451.domain.APIUser;
 import org.junit.After;
 import org.junit.Assert;
 import org.junit.Before;
 import org.junit.Test;
+import org.springframework.security.core.Authentication;
+import org.springframework.security.oauth2.provider.OAuth2Authentication;
 
 import java.security.Principal;
+import java.util.HashMap;
+import java.util.Map;
 
 import static org.mockito.Mockito.mock;
+import static org.mockito.Mockito.when;
 
 public class UserControllerTest {
 
@@ -20,10 +26,18 @@ public class UserControllerTest {
 
   @Test
   public void testUserControllerUserShouldReturnPassedInPrincipal() {
-    Principal principal = mock(Principal.class);
     UserController testUserController = new UserController();
+    OAuth2Authentication mockPrincipal = mock(OAuth2Authentication.class);
+    Authentication mockAuthentication = mock(Authentication.class);
+    Map<String, String> userDetails = new HashMap<String, String>();
 
-    Principal result = testUserController.user(principal);
-    Assert.assertEquals("User did not return the passed in principal", principal, result);
+    userDetails.put("name","testname");
+    userDetails.put("login","testusername");
+    when(mockAuthentication.getDetails()).thenReturn(userDetails);
+    when(mockPrincipal.getUserAuthentication()).thenReturn(mockAuthentication);
+
+    APIUser result = testUserController.user(mockPrincipal);
+    Assert.assertEquals("User did not have the correct name", "testname", result.getName());
+    Assert.assertEquals("User did not have the correct username", "testusername", result.getUsername());
   }
 }
