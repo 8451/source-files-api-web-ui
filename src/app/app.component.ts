3b1b8bfb6ea01bf46802cf0a51966c51ca6b0247
t413081 <taylor.brady@8451.com>
2017-03-22 00:54:34 -0400

    Fixed auth service not in app.component.ts file.

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,5 +1,6 @@
 import { Component } from '@angular/core';
 import { Router } from '@angular/router';
+import { AuthService } from './services/auth.service';
 
 @Component({
   selector: 'app-root',
@@ -11,7 +12,8 @@ export class AppComponent {
   routeLinks: any[];
   activeLinkIndex = 0;
 
-  constructor(private router: Router) {
+  constructor(private router: Router,
+              public authService: AuthService) {
     this.routeLinks = [
       { label: 'Profile', link: 'profile' },
       { label: 'Terms', link: 'terms' },
